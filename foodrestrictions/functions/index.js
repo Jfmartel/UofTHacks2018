dd5832eef39adfb3a02693ab141145ff5fabf48b
Jordan Martel <jfm_martel@hotmail.com>
2018-01-20 02:30:51 -0500

    formatting

--- foodrestrictions/functions/index.js
+++ foodrestrictions/functions/index.js
@@ -3,29 +3,27 @@
 const functions = require('firebase-functions');
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 
-const googleAssistantRequest = 'google';
-
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-        console.log('Request headers: ' + JSON.stringify(request.headers));
-console.log('Request body: ' + JSON.stringify(request.body));
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
 
-const app = new DialogflowApp({request: request, response: response});
-const WELCOME_INTENT = 'input.welcome';
-const INGREDIENT_INTENT = 'input.ingredient';
+    const app = new DialogflowApp({request: request, response: response});
+    const WELCOME_INTENT = 'input.welcome';
+    const INGREDIENT_INTENT = 'input.ingredient';
 
-function welcomeIntent (app) {
-    app.ask('Welcome to food restrictions. What allergies do you have?',
-        ['What ingredients are you allergic to?', 'What are you allergic to?', 'We can stop here. See you soon.']);
-}
+    function welcomeIntent (app) {
+        app.ask('Welcome to food restrictions. What allergies do you have?',
+            ['What ingredients are you allergic to?', 'What are you allergic to?', 'We can stop here. See you soon.']);
+    }
 
-function ingredientIntent (app) {
-    const ingredient = app.getArgument('ingredName');
-    app.tell('You said ' + ingredient);
-}
+    function ingredientIntent (app) {
+        const ingredient = app.getArgument('ingredName');
+        app.tell('You said ' + ingredient);
+    }
 
-const actionMap = new Map();
-actionMap.set(WELCOME_INTENT, welcomeIntent);
-actionMap.set(INGREDIENT_INTENT, ingredientIntent);
-app.handleRequest(actionMap);
+    const actionMap = new Map();
+    actionMap.set(WELCOME_INTENT, welcomeIntent);
+    actionMap.set(INGREDIENT_INTENT, ingredientIntent);
+    app.handleRequest(actionMap);
 
 })
